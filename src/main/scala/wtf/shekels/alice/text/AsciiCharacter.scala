60e6b8da015b208468cdec464938ea1d9207afe6
odisseus <odisseus@users.noreply.github.com>
2018-11-27 19:00:35 +0100

    Eliminated image files for rendered characters.

--- src/main/scala/wtf/shekels/alice/text/AsciiCharacter.scala
+++ src/main/scala/wtf/shekels/alice/text/AsciiCharacter.scala
@@ -2,41 +2,14 @@ package wtf.shekels.alice.text
 
 import java.awt.image.BufferedImage
 import java.awt.{Color, Font, Graphics}
-import java.io.File
 
 import com.sksamuel.scrimage.Image
 import com.sksamuel.scrimage.filter.GrayscaleFilter
-import javax.imageio.ImageIO
 import wtf.shekels.alice.Util
 
-/**
-  * Wrapper class for character that provides some extra functionality for lightness and reading/writing images
-  */
 class AsciiCharacter(val char: String, val font: String) {
 
-  /**
-    * Parent directory that contains the character images
-    */
-  val dir: File = new File("characters")
-
-  /**
-    * The File this character belongs to
-    */
-  val file: File = new File(s"characters/$char.png")
-
-  /**
-    * Write character to image & check if dir exists
-    */
-  def writeImage(): Unit = {
-    if (!dir.exists()) { // If dir doesn't exist make it
-      dir.mkdir()
-    } else if (!dir.isDirectory) { // If dir is a file warn the user
-      println("File named `characters` exists, will not create directory, move or rename this file")
-      System.exit(0)
-    }
-
-    // Else run like normal
-
+  private lazy val renderedImage: Image = {
     // Create new grayscale image to write our character to
     val image: BufferedImage = new BufferedImage(45, 70, BufferedImage.TYPE_BYTE_GRAY)
 
@@ -52,27 +25,13 @@ class AsciiCharacter(val char: String, val font: String) {
     graphics.setFont(new Font(font, Font.PLAIN, 75))
     graphics.drawString(char, 0, 60)
 
-    // Make sure parent dir exists
-    val parent: File = new File("characters")
-    if (!parent.exists()) parent.mkdir()
-
-    // Save the image to the file
-    ImageIO.write(image, "png", file)
+    Image.wrapAwt(image)
   }
 
-  /**
-    * Calculates the mean of the lightness values in the image
-    * Checks if files & directories exist
-    */
-  def calcLightness(): Double = {
-    if (file.exists()) {
-      val pixels = Image.fromFile(file).filter(GrayscaleFilter).pixels
-      val values = pixels.map(p => 255 - Util.rgbToLightness(p.toColor))
-      values.sum / values.length
-    } else {
-      writeImage()
-      calcLightness()
-    }
+  lazy val lightness: Double = {
+    val pixels = renderedImage.filter(GrayscaleFilter).pixels
+    val values = pixels.map(p => 255 - Util.rgbToLightness(p.toColor))
+    values.sum / values.length
   }
 
 }
